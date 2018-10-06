633246d3c08beaba84523130dfef432187f1e24f
Jérôme De Greef <j-degreef@users.noreply.github.com>
2018-10-06 18:27:02 +0200

    bug fix to avoid multiple beeps and remove supend thats not working (use dpms instead)

--- bin/bin/xautolock.sh
+++ bin/bin/xautolock.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-#if [ $(pgrep xautolock) ] ; then
-#    echo yes;
-#    killall xautolock ;
-#fi
-
-xautolock -exit # remove previous instance if any
-xset dpmx force suspend
-sleep 1
-xautolock -detectsleep \
-  -time 15 -locker i3_lock -notify 30 \
-  -notifier "play -n synth 0.1 sin 1090 || echo -e "\a";notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'" &
+if [[ ! $(pgrep -x xautolock) ]] ; then
+    echo "Starting xautolock..."
+    xautolock -detectsleep \
+        -time 10 -locker i3_lock -notify 30 \
+        -notifier "play -n synth 0.1 sin 1090 || echo -e "\a";notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'" &
+fi
 
